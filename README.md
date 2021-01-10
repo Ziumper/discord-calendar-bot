01bc667630f69f1665759e62e09b3af9549e9191
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2021-01-10 22:43:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Discord calendar bot app
+
+Example tempalte for discord calendar bot, ready to be hostend on [heroku](https://herokuapp.com)
+
+Link to hosted example: [Farmind-calendar-bot](https://farmind-bot-calendar.herokuapp.com/)
+
+Used [google calendar node js api](https://developers.google.com/calendar/quickstart/nodejs) , and get started [herokuapp](https://github.com/heroku/node-js-getting-started) with [nodejs](https://nodejs.org/en/)
---
+++ index.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//test
+const cooldowns = new Discord.Collection();
+client.commands = new Discord.Collection();
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+
+    //Handling for commands 
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    //Handlifn for commands if has no arguments
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    //handling for commands cooldowns
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+
+    //Execution for command
+    try {
+        command.execute(message, args);
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+
+client.login(token);
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
